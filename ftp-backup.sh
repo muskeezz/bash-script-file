a54adc957f7577e3bfa27eb70de5deb5b85e21cc
andre manatad <muskeezz@gmail.com>
2023-10-10 17:51:38 +0800

    ftp script version 4

--- ftp-backup.sh
+++ ftp-backup.sh
@@ -9,7 +9,7 @@
 # Version: v2
 #########################
 
-<<EOF
+<< 'EOF'
 
 # Version: v1
 # Variables
@@ -32,30 +32,21 @@
 #printf "`date '+%Y%m%d %H%M'` /var/opt/CrushFTP10/WebInterface/favicon.ico `rsync -azvn --exclude=lost+found --exclude=admin andre@10.10.10.187:/var/opt/CrushFTP10/WebInterface/favicon.ico /var/opt/CrushFTP10/WebInterface/ --stats | sed '0,/^$/d'` finish `date '+%Y%m%d %H%M'` \n\n`head -n 2000 ./ftp.log`" > ftp.log
 #printf "`date '+%Y%m%d %H%M'` /media/Logs `rsync -azvn --exclude=lost+found --exclude=admin andre@10.10.10.187:/media/Logs /media/Logs --stats | sed '0,/^$/d'` finish `date '+%Y%m%d %H%M'` \n\n`head -n 2000 ./ftp.log`" > ftp.log
 
-EOF
-
 # Version: v3
-#Variable
-
-USER="andre"
-REMOTE="10.10.10.187"
-REMOTE_SERVER="$USER@REMOTE"
-DATE=`date '+%d%m%Y'`
-DATE_TIME="DATE: `date '+%Y-%b-%m'` TIME: `date '+%I:%M %p'`"
-BACKUP1="/var/opt/CrushFTP10/users/MainUsers"
-BACKUP2="/var/opt/CrushFTP10/WebInterface/images
-BACKUP3="/var/opt/CrushFTP10/WebInterface/favicon.ico"
-BACKUP4="/media/Logs"
-OPT_EXCLUDE="-avzn --exclude={'lost+found','admin'}"
 
 #Function
-
 backup(){
-echo -e "$DATE_TIME | $1 \n---------------------------------------------\n\
-`rsync $OPT_EXCLUDE $REMOTE_SERVER:$1 $1 --stats | sed '0,/^$/d'`\n\
-Finish: $DATE_TIME \n---------------------------------------------\n\n\
-`head -n 2000 $HOME/crushftp/log/$DATE-ftp.log`" > $HOME/crushftp/log/$DATE-ftp.log
+echo -e "`date '+%Y-%b-%m %I:%M %p'` | $1 \n---------------------------------------------\n`\
+rsync -avzn --exclude={'lost+found','admin'} andre@10.10.10.187:$1 $1 --stats | \
+sed '0,/^$/d'`\nFinish: `date '+%Y-%b-%m %I:%M %p'` \n---------------------------------------------\n\n`\
+head -n 2000 $HOME/bash-script-file/ftp.log`" > $HOME/bash-script-file/ftp.log
 }
 
-backup $BACKUP1
+backup /var/opt/CrushFTP10/users/MainUsers
+backup /var/opt/CrushFTP10/WebInterface/images
+backup /var/opt/CrushFTP10/WebInterface/favicon.ico
+backup /media/Logs
 
+EOF
+
+# Version: v4
