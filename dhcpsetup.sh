d7ee6290734a4987fba1530fb115aaef672100b2
andre manatad <muskeezz@gmail.com>
2023-11-19 20:28:02 +0800

    dhcpsetup.sh update

--- dhcpsetup.sh
+++ dhcpsetup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Root privilege is required
 
 # Disable systemd-resolved
 sudo systemctl stop systemd-resolved
@@ -7,40 +6,45 @@ sudo systemctl disable systemd-resolved
 sudo unlink /etc/resolv.conf
 
 # Hard code the nameserver
-sudo bash -c 'echo -e "nameserver 127.0.0.1\nnameserver 1.1.1.1\nnameserver 8.8.8.8" >/etc/resolv.conf'
+nm1="1.1.1.1"
+nm2="8.8.8.8"
+echo "nameserver 127.0.0.1\nnameserver $nm1\nnameserver $nm2" >~/resolv.conf
 
 # Install dnsmasq
 sudo apt-get update
 sudo apt-get install dnsmasq -y
 
 # Configure dnsmasq
-[ ! -e /etc/dnsmasq.conf.orig] && sudo mv -v /etc/dnsmasq.conf /etc/dnsmasq.conf.orig 2>/dev/null
+[ ! -e /etc/dnsmasq.conf.orig] && mv -v /etc/dnsmasq.conf /etc/dnsmasq.conf.orig 2>/dev/null
 
-l_interface=`ip a | grep -w 'inet' | grep -v 'dynamic' | tail -1 | awk '{print $7}'`
-subnet=`ip a | grep -w 'inet' | grep -v 'dynamic' | tail -1 | awk '{print $2}'`
-ip=`echo $subnet | awk -F '/' '{print $1}'`
-sz=`echo $subnet | awk -F '/' '{print $2}'`
+interface=`ip a | grep -w 'inet' | grep -v 'dynamic' | tail -1 | awk '{print $7}'`
+ip=`ip a | grep -w 'inet' | grep -v 'dynamic' | tail -1 | awk '{print $2}' | awk -F '/' '{print $1}'`
 segment=`echo ${ip%.*}`
 router=`ip route show | head -1 | awk '{print $3}'`
-
+netboot="192.168.18.200"            
 fileBIOS="netboot.xyz.kpxe"
 fileUEFI="netboot.xyz.efi"
 
-sudo bash -c 'echo -e "interface='$l_interface'\nbind-interfaces\n\
-domain=\n\
-\n\
-dhcp-range='$l_interface','$segment'.110,'$segment'.120,255.255.255.0,8h\n\
-dhcp-option=option:router,'$router'\n\
-dhcp-option=option:dns-server,1.1.1.1\n\
-dhcp-option=option:dns-server,8.8.8.8\n\
-\n\
-dhcp-match=set:bios-x86,option:client-arch,0\n\
-dhcp-boot=tag:bios-x86,'$fileBIOS'\n\
-\n\
-dhcp-match=set:efi-x86_64,option:client-arch,7\n\
-dhcp-match=set:efi-x86_64,option:client-arch,9\n\
-dhcp-boot=tag:efi-x86_64,'$fileUEFI'\n\
-" > /etc/dnsmasq.conf'
+echo "port=0\ninterface=$interface\nbind-dynamic\n\
+log-dhcp\ndhcp-authoritative\n\
+dhcp-range=$segment.2,$segment.254,255.255.255.0,8h\n\
+dhcp-option=option:router,$router\n\
+dhcp-option=option:dns-server,$nm1\n\
+dhcp-option=option:dns-server,$nm2\n\
+dhcp-match=set:bios,option:client-arch,0\n\
+dhcp-match=set:efi-i32,option:client-arch,2\n\
+dhcp-match=set:efi-i32,option:client-arch,6\n\
+dhcp-match=set:efi-i64,option:client-arch,7\n\
+dhcp-match=set:efi-i64,option:client-arch,8\n\
+dhcp-match=set:efi-i64,option:client-arch,9\n\
+dhcp-match=set:efi-a32,option:client-arch,10\n\
+dhcp-match=set:efi-a64,option:client-arch,11\n\
+dhcp-boot=tag:bios,$fileBIOS,,$netboot\n\
+dhcp-boot=tag:!bios,$fileUEFI,,$netboot" > ~/dnsmasq.conf
+
+chmod 644 ~/dnsmasq.conf ~/resolv.conf
+sudo chown root:root ~/dnsmasq.conf ~/resolv.conf
+sudo mv -v ~/dnsmasq.conf ~/resolv.conf /etc/
+
 sudo systemctl restart dnsmasq
 sudo systemctl enable dnsmasq
-sudo systemctl status dnsmasq
