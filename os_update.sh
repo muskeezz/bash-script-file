4318299c7980213bce8d3ddfe67f1ffbeeabf24c
Andre Manataad <muskeezz@gmail.com>
2023-11-21 13:57:04 +0000

    git_connect.sh

--- os_update.sh
+++ os_update.sh
@@ -4,13 +4,13 @@ distro=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2)
 
 case "$distro" in
     "ubuntu" | "debian")
-        sudo apt update && sudo apt upgrade -y
+        sudo apt update && sudo apt upgrade
         ;;
     "centos" | "fedora" | "rhel")
-        sudo yum update -y
+        sudo yum update
         ;;
     "opensuse" | "sles")
-        sudo zypper refresh && sudo zypper update -y
+        sudo zypper refresh && sudo zypper update
         ;;
     *)
         echo "Unsupported distribution: $distro"
