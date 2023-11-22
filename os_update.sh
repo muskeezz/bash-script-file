18f911218d33c943855c9d4cca2814d6c17fb21e
andre manatad <muskeezz@gmail.com>
2023-11-22 23:40:06 +0800

    asd

--- os_update.sh
+++ os_update.sh
@@ -6,26 +6,35 @@ cleanup(){
     rm -f dpkg-installed-*.log
     rm -f upgrade-*.log
 }
+trap cleanup EXIT
 _lock=".lock"
 [ -f .lock ] && exit 0 || touch "$_lock"
 
 check_new_kernel(){
-    local latest_kernel=$(apt list --upgradable 2>/dev/null | grep -oP 'linux-image-\d+\.\d+\.\d+-\d+' | sort -V | tail -1)
-    local current_kernel=$(uname -r)
-    if [[ $latest_kernel > $current_kernel ]]; then
-        echo "A new kernel version ($latest_kernel) is available for upgrade."
+#    local latest_kernel=$(apt list --upgradable 2>/dev/null | grep -oP 'linux-image-\d+\.\d+\.\d+-\d+' | sort -V | tail -1)
+#    local current_kernel=$(uname -r)
+    local new_kernel=$(sudo apt list --upgradable | grep linux-image | wc -l)
+
+    if [[ $new_kernel -eq 0 ]]; then
+        #echo "A new kernel version ($latest_kernel) is available for upgrade."
+        echo "A new kernel version is available for upgrade."
     fi
 }
-
+check_new_package(){
+    sudo apt update
+    local upgrade=$(sudo apt list --upgradable | wc -l)
+    if [[ $upgrade -gt 1 ]]; then
+        sudo dpkg -l > dpkg-installed-$(date '+%Y%m%d').log 2>&1
+        sudo apt list --upgradable > upgrade-$(date '+%Y%m%d').log 2>&1
+        echo "New Package available. Please see upgrade-$(date '+%Y%m%d').log"
+    fi
+}
 
 distro=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2)
 
 case "$distro" in
     "ubuntu" | "debian")
-        sudo apt update > update-$(date '+%Y%m%d').log 2>&1
-        sudo dpkg -l > dpkg-installed-$(date '+%Y%m%d').log 2>&1
-        sudo apt list --upgradable > upgrade-$(date '+%Y%m%d').log 2>&1
-        echo "New Package available. Please see upgrade-$(date '+%Y%m%d').log"
+        check_new_package
         check_new_kernel
         ;;
     "centos" | "fedora" | "rhel")
@@ -39,4 +48,4 @@ case "$distro" in
         ;;
 esac
 
-rm -f "$_lock"
+#rm -f "$_lock"
